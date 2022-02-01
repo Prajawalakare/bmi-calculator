dc10965ddb537290381f384b9f704ce279d84ec6
Prajawalakare <prajwal1113@gmail.com>
2022-02-02 01:47:59 +0530

    Remark

--- js/script.js
+++ js/script.js
@@ -7,5 +7,17 @@ function calculateBMI(){
     var bmi=(weight/(heightm*heightm)).toFixed(2);
 
     document.getElementById("txt-bmi").innerHTML="BMI: "+bmi;
-    
+
+    if(bmi<18.5){
+        document.getElementById("txt-remark").innerHTML="Underweight";
+    }
+    else if(bmi>=18.5 && bmi<=24.9){
+        document.getElementById("txt-remark").innerHTML="Normal";
+    }
+    else if(bmi>=25 && bmi<=29.9){
+        document.getElementById("txt-remark").innerHTML="overweight";
+    }
+    else if(bmi>=30){
+        document.getElementById("txt-remark").innerHTML="Obese";
+    }
 }
